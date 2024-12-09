5595d3c900347b62d338d84f871ecd64d9683b34
addis-tech <adisu.ketema@gmail.com>
2024-12-09 18:19:35 +0300

    Deploying to netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-e66ef/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-e66ef/us-central1/api",
 
   // Deployed version of amazon server on render.com
   baseURL: "https://amazon-api-deploy-1-a27q.onrender.com"
